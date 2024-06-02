6e43f21ad24060706a418482b9807e050a4d869c
Supanee Patsa <156906258+Supanee-patsa@users.noreply.github.com>
2024-06-02 14:03:25 +0200

    add script.js

---
+++ script/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+         $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
+
+$('#bhromaon-btn').on('click', ()=> {
+    $('.bhromaon-show-text').slideToggle(1000); 
+    let x=document.getElementById("bhromaon-btn"); 
+    if(x.innerHTML==="Show more"){
+        x.innerHTML = "Show less"; 
+    } else {
+        x.innerHTML = "Show more"; 
+    }
+});
+
+
+$('#genesis-btn').on('click', ()=> {
+    $('.genesis-show-text').slideToggle(1000); 
+    let x=document.getElementById("genesis-btn"); 
+    if(x.innerHTML==="Show more"){
+        x.innerHTML = "Show less"; 
+    } else {
+        x.innerHTML = "Show more"; 
+    }
+});
+
+
+
